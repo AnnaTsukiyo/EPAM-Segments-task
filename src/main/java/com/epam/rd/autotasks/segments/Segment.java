7177a4889cb520e15b28f41af13f2580c07145f0
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-27 22:24:22 +0200

    amended

--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -5,15 +5,13 @@ import static java.lang.Math.sqrt;
 import static java.lang.StrictMath.pow;
 
 class Segment {
+
     private Point start;
     private Point end;
 
     public Segment(Point start, Point end) throws IllegalArgumentException {
-        if (start == null || end == null) {
-            throw new IllegalArgumentException("Arguments can't be null");
-        }
-        if (start.equals(end)) {
-            throw new IllegalArgumentException("The points must differ");
+        if (start == null || end == null || start.equals(end)) {
+            throw new IllegalArgumentException();
         } else {
             this.start = start;
             this.end = end;
@@ -32,23 +30,24 @@ class Segment {
     }
 
     Point intersection(Segment another) {
-        double A1 = this.end.getY() - this.start.getY();
-        double B1 = this.start.getX() - this.end.getX();
-        double C1 = A1 * this.start.getX() + B1 * this.start.getY();
 
-        double A2 = another.end.getY() - another.start.getY();
-        double B2 = another.start.getX() - another.end.getX();
-        double C2 = A2 * another.start.getX() + B2 * another.start.getY();
+        another = new Segment(new Point(start.getX(), start.getY()), new Point(end.getX(), end.getY()));
+        Segment segment = new Segment(new Point(this.start.getX(), this.start.getY()), new Point(this.end.getX(), this.end.getY()));
 
-        double det = (A1 * B2) - (A2 * B1);
+        double A1 = another.end.getY() - another.start.getY();
+        double B1 = another.start.getX() - another.end.getX();
+        double C1 = A1 * another.start.getX() + B1 * another.start.getY();
+
+        double A2 = this.end.getY() - this.start.getY();
+        double B2 = this.start.getX() - this.end.getX();
+        double C2 = A2 * this.start.getX() + B2 * this.start.getY();
+
+        double det = A1 * B2 - A2 * B1;
+        if (det == 0) {
+            return null;
+        }
         double x = (B2 * C1 - B1 * C2) / det;
         double y = (A1 * C2 - A2 * C1) / det;
-        if ((this.end == another.end && this.start != another.start) || (this.start == another.start && this.end == another.end)) {
-            return null;
-        } else {
-            return new Point((int) x, (int) y);
-        }
+        return new Point(x, y);
     }
 }
-
-
