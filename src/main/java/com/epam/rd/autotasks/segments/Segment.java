0c94ad3d22b35926de966d88ea0069b6088b7e39
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-28 18:05:26 +0200

    amended

--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -5,13 +5,15 @@ import static java.lang.Math.sqrt;
 import static java.lang.StrictMath.pow;
 
 class Segment {
+    Point start;
+    Point end;
 
     public Segment(Point start, Point end) throws RuntimeException {
-        if (!start.equals(end)) {
+        if (start.equals(end)) {
+            throw new RuntimeException();
+        } else {
             this.start = start;
             this.end = end;
-        } else {
-            throw new RuntimeException();
         }
     }
 
@@ -25,31 +27,23 @@ class Segment {
     }
 
     Point intersection(Segment another) {
-        double k1 = (end.getY() - start.getY() / (end.getX() - start.getX()));
-        double k2 = (another.end.getY() - another.start.getY() / (another.end.getX() - another.start.getX()));
-        if (k1 == k2) {
+        if ((end.getY() - start.getY() / end.getX() - start.getX()) == another.end.getY() - another.start.getY() / another.end.getX() - another.start.getX()) {
             return null;
         }
-        double x1 = start.getX();
-        double x2 = end.getX();
-        double x3 = another.start.getX();
-        double x4 = another.end.getX();
-        double y1 = start.getY();
-        double y2 = end.getY();
-        double y3 = another.start.getY();
-        double y4 = another.end.getY();
-        double inY;
-        double inX;
-        double denom = (((y4 - y3) * (x2 - x1)) - ((x4 - x3) * (y2 - y1)));
-        if (denom == 0) {
-            return null;
-        }
+        double A1 = another.end.getY() - another.start.getY();
+        double B1 = another.start.getX() - another.end.getX();
+        double C1 = A1 * another.start.getX() + B1 * another.start.getY();
 
-        double u1 = (((x4 - x3) * (y1 - y3)) - ((y4 - y3) * (x1 - x3))) / denom;
-        double u2 = (((x2 - x1) * (y1 - y3)) - ((y2 - y1) * (x1 - x3))) / denom;
-        if (u1 > 1 || u1 < 0 || u2 > 1 || u2 < 0) {
+        double A2 = end.getY() - start.getY();
+        double B2 = start.getX() - end.getX();
+        double C2 = A2 * start.getX() + B2 * start.getY();
+
+        double det = A1 * B2 - A2 * B1;
+        if (det == 0) {
             return null;
-            }
-        return new Point(inX, inY);
+        }
+        double x = (B2 * C1 - B1 * C2) / det;
+        double y = (A1 * C2 - A2 * C1) / det;
+        return new Point(x, y);
     }
 }
